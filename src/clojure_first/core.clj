02a99808a41f8c0f1fbab9e1722dda2d6dec9e34
Pedro Banali <pedro_banali@hotmail.com>
2017-07-22 18:15:17 -0300

    First Commit

---
+++ src/clojure_first/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-first.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hey Clojure up and running"))
---
+++ test/clojure_first/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-first.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-first.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
